ac04b1838f2403c8706d28c6bae59db83529ec9e
Rob Berwick <rob.berwick@gmail.com>
2024-06-29 14:58:27 +0100

    Use AdsrComponent class in PluginEditor

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -1,15 +1,12 @@
 #include "PluginEditor.h"
 
 PluginEditor::PluginEditor (PluginProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+    : AudioProcessorEditor (&p), processorRef (p), adsr (processorRef.apvts)
 {
     juce::ignoreUnused (processorRef);
 
     addAndMakeVisible (inspectButton);
-    attackAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "ATTACK", attackSlider);
-    decayAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "DECAY", decaySlider);
-    sustainAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "SUSTAIN", sustainSlider);
-    releaseAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "RELEASE", releaseSlider);
+
     oscAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment> (processorRef.apvts, "OSC", oscSelector);
 
     // this chunk of code instantiates and opens the melatonin inspector
@@ -23,10 +20,8 @@ PluginEditor::PluginEditor (PluginProcessor& p)
         inspector->setVisible (true);
     };
 
-    setSliderParams (attackSlider);
-    setSliderParams (decaySlider);
-    setSliderParams (sustainSlider);
-    setSliderParams (releaseSlider);
+    // make adsr component visible
+    addAndMakeVisible (adsr);
 
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
@@ -55,24 +50,9 @@ void PluginEditor::resized()
     auto bounds = getLocalBounds().reduced (10);
     auto header = bounds.removeFromTop (50);
     auto footer = bounds.removeFromBottom (50);
-    auto oscSelectorBounds = bounds;
+    adsr.setBounds (bounds);
     inspectButton.setBounds (header.withSizeKeepingCentre (100, 50));
 
-    constexpr auto padding = 10;
-    const auto sliderWidth = oscSelectorBounds.getWidth() / 4;
-    const auto sliderHeight = oscSelectorBounds.getHeight() - (padding);
-    const auto sliderStartX = oscSelectorBounds.getX();
-    const auto sliderStartY = oscSelectorBounds.getHeight() / 2 - (sliderHeight / 2);
+    // set adsr component bounds
 
-    attackSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
-    decaySlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
-    sustainSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
-    releaseSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
-
-}
-void PluginEditor::setSliderParams (juce::Slider& slider)
-{
-    slider.setSliderStyle (juce::Slider::LinearVertical);
-    slider.setTextBoxStyle (juce::Slider::TextBoxBelow, true, 50, 25);
-    addAndMakeVisible (slider);
 }
--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -3,6 +3,7 @@
 #include "PluginProcessor.h"
 #include "BinaryData.h"
 #include "melatonin_inspector/melatonin_inspector.h"
+#include "ui/AdsrComponent.h"
 
 //==============================================================================
 class PluginEditor : public juce::AudioProcessorEditor
@@ -16,25 +17,15 @@ public:
     void resized() override;
 
 private:
-    void setSliderParams( juce::Slider& slider);
-
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     PluginProcessor& processorRef;
+    AdsrComponent adsr;
 
-    juce::Slider attackSlider;
-    juce::Slider decaySlider;
-    juce::Slider sustainSlider;
-    juce::Slider releaseSlider;
+
     juce::ComboBox oscSelector;
-    juce::Slider gainSlider;
 
-    using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
-    std::unique_ptr<SliderAttachment> attackAttachment;
-    std::unique_ptr<SliderAttachment> decayAttachment;
-    std::unique_ptr<SliderAttachment> sustainAttachment;
-    std::unique_ptr<SliderAttachment> releaseAttachment;
-    std::unique_ptr<SliderAttachment> gainAttachment;
+
 
     std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> oscAttachment;
 
