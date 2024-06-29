56b617a0d2d7d0f732fd80f0052fae1b99687a6a
Rob Berwick <rob.berwick@gmail.com>
2024-06-29 12:53:58 +0100

    tweak adsr slider layout

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -10,7 +10,6 @@ PluginEditor::PluginEditor (PluginProcessor& p)
     decayAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "DECAY", decaySlider);
     sustainAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "SUSTAIN", sustainSlider);
     releaseAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "RELEASE", releaseSlider);
-    gainAttachment = std::make_unique<SliderAttachment> (processorRef.apvts, "GAIN", gainSlider);
     oscAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment> (processorRef.apvts, "OSC", oscSelector);
 
     // this chunk of code instantiates and opens the melatonin inspector
@@ -53,22 +52,22 @@ void PluginEditor::paint (juce::Graphics& g)
 void PluginEditor::resized()
 {
     // layout the positions of your child components here
-    auto area = getLocalBounds().reduced (10);
-    area.removeFromBottom (50);
-    inspectButton.setBounds (getLocalBounds().withSizeKeepingCentre (100, 50));
+    auto bounds = getLocalBounds().reduced (10);
+    auto header = bounds.removeFromTop (50);
+    auto footer = bounds.removeFromBottom (50);
+    auto oscSelectorBounds = bounds;
+    inspectButton.setBounds (header.withSizeKeepingCentre (100, 50));
 
     constexpr auto padding = 10;
-    const auto sliderWidth = area.getWidth() / 5 - (2 * padding);
-    const auto sliderHeight = area.getHeight() - (2 * padding);
-    constexpr auto sliderStartX = padding;
-    const auto sliderStartY = area.getHeight() / 2 - (sliderHeight / 2);
+    const auto sliderWidth = oscSelectorBounds.getWidth() / 4;
+    const auto sliderHeight = oscSelectorBounds.getHeight() - (padding);
+    const auto sliderStartX = oscSelectorBounds.getX();
+    const auto sliderStartY = oscSelectorBounds.getHeight() / 2 - (sliderHeight / 2);
 
-    attackSlider.setBounds (sliderStartX, sliderStartY, sliderWidth, sliderHeight);
-    decaySlider.setBounds (attackSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-    sustainSlider.setBounds (decaySlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-    releaseSlider.setBounds (sustainSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
-
-    gainSlider.setBounds (releaseSlider.getRight() + padding, sliderStartY, sliderWidth, sliderHeight);
+    attackSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
+    decaySlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
+    sustainSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
+    releaseSlider.setBounds (oscSelectorBounds.removeFromLeft (sliderWidth));
 
 }
 void PluginEditor::setSliderParams (juce::Slider& slider)
