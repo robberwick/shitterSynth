82150703d0e72f7b0bb2728a13a66e933e2a9832
Rob Berwick <rob.berwick@gmail.com>
2024-06-29 15:15:45 +0100

    remove redundant code from PluginEditor paint method

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -37,11 +37,7 @@ void PluginEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    auto area = getLocalBounds();
     g.setColour (juce::Colours::white);
-    g.setFont (16.0f);
-    // auto helloWorld = juce::String ("Hello from ") + PRODUCT_NAME_WITHOUT_VERSION + " v" VERSION + " running in " + CMAKE_BUILD_TYPE;
-    // g.drawText (helloWorld, area.removeFromTop (150), juce::Justification::centred, false);
 }
 
 void PluginEditor::resized()
